249664118df83e08be512e4cb65a95501a75eaa6
lakhe <249052680+0xlakhe@users.noreply.github.com>
2026-01-30 12:10:07 +0545

    complete component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import React, { useState } from "react";
-import Products from "./Products";
+import React from "react";
+import Card from "./components/Card";
+import User from "./components/User";
 
-function App() {
-  let [a, b] = useState(0);
+const App = () => {
   return (
-    <div className="text-white w-full h-screen bg-zinc-900">
-      <h1>{a}</h1>
-      <button
-        onClick={() => a < 67 && b(a + 1)}
-        className="rounded-md bg-green-500 p-2 text-white "
-      >
-        Click me
-      </button>
-      <Products name="value" count={a} />
+    <div className="bg-zinc-500 h-screen p-20">
+      <Card />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Bookmark } from "lucide-react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="parent w-100 h-102 bg-zinc-900 text-white rounded-xl">
+        <div className="top flex justify-between text-white p-5 w-full">
+          <img
+            className="w-20 h-20 rounded-[50%]"
+            src="https://images.unsplash.com/photo-1769399287930-7cdddd98ef3a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="saves">
+            <button className="flex opacity-50 hover:cursor-pointer hover:border-1 p-[4px] px-1 rounded-md hover:bg-gray-500">
+              Save <Bookmark />
+            </button>
+          </div>
+        </div>
+        <div className="center p-4 flex flex-col gap-2">
+          <div className="flex gap-1">
+            <h3>Google</h3>
+            <h6 className="self-end pb-[2px] text-[13px] opacity-50">
+              3 days ago
+            </h6>
+          </div>
+          <div className="text-[20px]">
+            <h1>Senior UI/UX Designer</h1>
+          </div>
+          <div className="tags flex gap-2 text-[13px] opacity-50">
+            <h5 className="border-1 border-1 rounded-sm p-[3px] bg-gray-600">
+              Part-time
+            </h5>
+            <h5 className="border-1 border-1 border-1 rounded-sm p-[3px] bg-gray-600">
+              Senior Level
+            </h5>
+          </div>
+        </div>
+        <div className="bottom border-t-2 border-gray-100/50 flex justify-between mt-20 p-5">
+          <div>
+            <h1>$120/hr</h1>
+            <h4 className="text-[13px] opacity-50">Mumbai, India</h4>
+          </div>
+          <button className="border-1 px-3 rounded-xl hover:cursor-pointer hover:bg-green-900">
+            Apply now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
